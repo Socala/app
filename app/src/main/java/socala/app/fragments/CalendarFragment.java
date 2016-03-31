bc363a26701e1bbee3ab5519c2c9c52dda8dad69
bromano <benjamin.romano@outlook.com>
2016-03-31 15:50:23 -0500

    Added WeekView into calendar fragment and updated event

--- app/src/main/java/socala/app/fragments/CalendarFragment.java
+++ app/src/main/java/socala/app/fragments/CalendarFragment.java
@@ -1,16 +1,27 @@
 package socala.app.fragments;
 
 
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import socala.app.R;
 
-public class CalendarFragment extends Fragment {
+public class CalendarFragment extends Fragment implements WeekView.EventClickListener, MonthLoader.MonthChangeListener, WeekView.EventLongPressListener {
+
+    @Bind(R.id.weekView) WeekView weekView;
 
     public CalendarFragment() {
     }
@@ -23,6 +34,25 @@ public class CalendarFragment extends Fragment {
 
         ButterKnife.bind(this, view);
 
+        weekView.setOnEventClickListener(this);
+        weekView.setMonthChangeListener(this);
+        weekView.setEventLongPressListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onEventClick(WeekViewEvent event, RectF eventRect) {
+
+    }
+
+    @Override
+    public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+        return new ArrayList<WeekViewEvent>();
+    }
+
+    @Override
+    public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
+
     }
 }
--- app/src/main/java/socala/app/models/Event.java
+++ app/src/main/java/socala/app/models/Event.java
@@ -1,11 +1,74 @@
 package socala.app.models;
 
+import java.util.Date;
+
 public class Event {
     private String id;
     private PrivacyLevel privacyLevel;
     private boolean rsvpable;
+    private String[] attendees;
+    private Date start;
+    private Date end;
+    private String[] recurrence;
+    private String recurringEventId;
+    private String location;
+    private String color;
 
-    // TODO: Add more properties. Need to discuss with CJ about this
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String[] getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(String[] attendees) {
+        this.attendees = attendees;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public String[] getRecurrence() {
+        return recurrence;
+    }
+
+    public void setRecurrence(String[] recurrence) {
+        this.recurrence = recurrence;
+    }
+
+    public String getRecurringEventId() {
+        return recurringEventId;
+    }
+
+    public void setRecurringEventId(String recurringEventId) {
+        this.recurringEventId = recurringEventId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
     public String getId() {
         return id;
